d674567a586f32a727be5ffe2ed5284f21cb3a42
annamarcomini <anajuliamarcomini23@gmail.com>
2026-02-12 10:41:57 -0300

    fix: create a schema users

--- createTable1.sql
+++ createTable1.sql
@@ -1,10 +1,10 @@
 -- Cria a tabela se não existe
-CREATE TABLE IF NOT EXISTS users(name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password_hash TEXT NOT NULL );
+CREATE TABLE IF NOT EXISTS users.user(name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password_hash TEXT NOT NULL );
 -- insere valores na tabela em cada campo desse
-INSERT INTO users(name, email, password_hash) 
+INSERT INTO users.user(name, email, password_hash) 
   VALUES (
   'João',
   'joao@email.com',
   '$2b$12$N9qo8uLOickgx2ZMRZo5e.u6a8rM6x5jvZ1pYp8q6B5X0X5p6K9W'
 );
-SELECT * FROM users;
+SELECT * FROM users.user;
