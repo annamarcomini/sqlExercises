f779e9e0c29c2626e0f26fd3d3337f582ee4331d
annamarcomini <anajuliamarcomini23@gmail.com>
2026-02-16 12:44:38 -0300

    fix: edit code

--- exercise 2/ddl.sql
+++ exercise 2/ddl.sql
@@ -4,7 +4,10 @@ CREATE TABLE IF NOT EXISTS exercicio2.product(
   name VARCHAR(50) NOT NULL, 
   description VARCHAR(10000) NOT NULL,
   quantity INTEGER NOT NULL,
-  price NUMERIC(10,2) NOT NULL
+  price NUMERIC(10,2) NOT NULL,
+  
+   --declaração de chave primária
+  CONSTRAINT exercicio2_product_pk PRIMARY KEY (id),
 
   --declaração de chaves únicas
   CONSTRAINT exercicio2_product_uq_barcode UNIQUE (barcode)
@@ -14,8 +17,11 @@ CREATE TABLE IF NOT EXISTS exercicio2.user(
   id INTEGER GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(40) NOT NULL, 
   email VARCHAR(50) NOT NULL, 
-  password_hash VARCHAR(200) NOT NULL 
+  password_hash VARCHAR(200) NOT NULL,
   
+  --declaração de chave primária
+  CONSTRAINT exercicio2_user_pk PRIMARY KEY (id),
+
   --declaração de chaves únicas
-  CONSTRAINT exercicio2_user_email_uq UNIQUE (email)
+  CONSTRAINT exercicio2_user_uq_email UNIQUE (email)
 );
