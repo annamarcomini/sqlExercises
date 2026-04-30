10d294e6e697d0bada333ed5ac882bffa34e93fd
annamarcomini <anajuliamarcomini23@gmail.com>
2026-04-30 14:20:07 -0300

    feat: create roles

---
+++ exercise7/roles.sql
@@ -0,0 +1,41 @@
+------ admin role -------
+CREATE ROLE admin;
+GRANT USAGE ON SCHEMA exercicio7 TO admin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA exercicio7 TO admin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA exercicio7 TO admin;
+
+------ chef role -------
+CREATE ROLE exercicio7.chef;
+GRANT USAGE ON SCHEMA exercicio7 TO chef;
+
+-- chefs table
+GRANT SELECT, UPDATE ON exercicio7.chefs TO chef;
+
+-- menus table
+GRANT SELECT, INSERT, UPDATE ON exercicio7.exercicio7.menus TO chef;
+
+-- reservations table
+GRANT SELECT, INSERT ON exercicio7.reservations TO chef;
+
+-- stations table
+GRANT SELECT ON exercicio7.stations TO chef;
+
+-- kitchens table
+GRANT SELECT ON exercicio7.kitchens TO chef;
+
+-- orders table
+GRANT SELECT ON exercicio7.orders TO chef;
+
+------- client role -------
+CREATE ROLE exercicio7.client;
+GRANT USAGE ON SCHEMA exercicio7 TO client;
+
+-- menus table
+GRANT SELECT ON exercicio7.menus TO client;
+
+-- orders table
+GRANT SELECT, INSERT ON exercicio7.orders TO client;
+
+------ Serial or identity accesss ------
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA exercicio7 
+TO chef, client;
