538cfd349dd882540fead2c57f2483996fb6456b
annamarcomini <anajuliamarcomini23@gmail.com>
2026-05-05 23:39:50 -0300

    fix: quotation marks

--- tests/exercise7/checks.test.sql
+++ tests/exercise7/checks.test.sql
@@ -9,7 +9,7 @@ SELECT lives_ok(
     INSERT INTO exercicio7.kitchens
       (name, city, street, number, neighborhood, state, zip_code, max_stations_number)
     VALUES
-      ('cozinha 1', 'Ribeirão', 'Rua 1', '104', 'Jardim clara', "SP", "19909-000", 10);
+      ('cozinha 1', 'Ribeirão', 'Rua 1', '104', 'Jardim clara', 'SP', '19909-000', 10);
   $$,
   'Permite valor total positivo'
 );
@@ -19,7 +19,7 @@ SELECT throws_ok(
     INSERT INTO exercicio7.kitchens
       (name, city, street, number, neighborhood, state, zip_code, max_stations_number)
     VALUES
-      ('cozinha 1', 'Ribeirão', 'Rua 1', '104', 'Jardim clara', "SP", "19909-000", -10);
+      ('cozinha 1', 'Ribeirão', 'Rua 1', '104', 'Jardim clara', 'SP', '19909-000', -10);
   $$,
   '23514',
   'violates check constraint',
