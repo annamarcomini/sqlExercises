b80b9d50d2ffd0e7e960ecdb7bbb37bb8bdfd178
annamarcomini <anajuliamarcomini23@gmail.com>
2026-05-07 10:43:30 -0300

    fix: sql erros

--- tests/exercise7/fn-validate-blank-strings.test.sql
+++ tests/exercise7/fn-validate-blank-strings.test.sql
@@ -1,39 +1,38 @@
 BEGIN;
 
-SET search_path to ext_pgtap;
+    SET search_path TO ext_pgtap;
 
-SELECT plan(5);
+  SELECT plan(5);
 
--- string normal
-SELECT ok(
+  -- string normal
+  SELECT ok(
     exercicio7.fn_validate_blank_strings('Joao'),
     'Texto comum deve retornar true'
-);
+  );
 
--- string com espaços externos
-SELECT ok(
+  -- string com espaços externos
+  SELECT ok(
     exercicio7.fn_validate_blank_strings('   Maria   '),
     'Texto com espaços nas bordas deve retornar true'
-);
+  );
 
--- string vazia
-SELECT ok(
+  -- string vazia
+  SELECT ok(
     NOT exercicio7.fn_validate_blank_strings(''),
     'String vazia deve retornar false'
-);
+  );
 
--- apenas espaços
-SELECT ok(
+  -- apenas espaços
+  SELECT ok(
     NOT exercicio7.fn_validate_blank_strings('     '),
     'String só com espaços deve retornar false'
-);
+  );
 
--- NULL
-SELECT ok(
+  -- NULL
+  SELECT ok(
     NOT exercicio7.fn_validate_blank_strings(NULL),
     'NULL deve retornar false'
-);
+  );
 
-SELECT * FROM finish();
-
+  SELECT * FROM finish();
 ROLLBACK;
--- tests/exercise7/fn-validate-cpf.test.sql
+++ tests/exercise7/fn-validate-cpf.test.sql
@@ -5,13 +5,13 @@ SET search_path to ext_pgtap;
 SELECT plan(3);
 
 SELECT diag('Retorna TRUE se o cpf for válido');
-SELECT is(exercicio7.validate_cpf('39053344705'), TRUE, 'CPF válido');
+SELECT is(exercicio7.fn_validate_cpf('39053344705'), TRUE, 'CPF válido');
 
 SELECT diag('CPF com digitos repetidos deve ser inválido');
-SELECT is(exercicio7.validate_cpf('11111111111'), FALSE, 'CPF com dígitos repetidos');
+SELECT is(exercicio7.fn_validate_cpf('11111111111'), FALSE, 'CPF com dígitos repetidos');
 
 SELECT diag('CPF com os digitos verificadores errados deve ser inválido');
-SELECT is(exercicio7.validate_cpf('39053344716'), FALSE, 'CPF com dígitos verificadores errados');
+SELECT is(exercicio7.fn_validate_cpf('39053344716'), FALSE, 'CPF com dígitos verificadores errados');
 
 SELECT * FROM finish();
 
