2250c625bef553770c58b730960b0ca26e3970f8
annamarcomini <anajuliamarcomini23@gmail.com>
2026-06-01 17:49:09 -0300

    fix: function random identifier

--- exercise7/fns/fn-generate-random-kitchen-identifier.sql
+++ exercise7/fns/fn-generate-random-kitchen-identifier.sql
@@ -3,12 +3,27 @@ RETURNS TEXT
 LANGUAGE plpgsql
 AS $$
 DECLARE
-  chars TEXT := 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  result TEXT := '';
+    chars TEXT := 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    result TEXT;
 BEGIN
-  FOR i IN 1..3 LOOP
-    result := result || substr(chars, floor(random() * length(chars) + 1)::int, 1);
-  END LOOP;
-  RETURN result;
-END
-$$
+    LOOP
+        result := '';
+
+        FOR i IN 1..3 LOOP
+            result := result || substr(
+                chars,
+                floor(random() * length(chars) + 1)::int,
+                1
+            );
+        END LOOP;
+
+        IF NOT EXISTS (
+            SELECT 1
+            FROM exercicio7.stations
+            WHERE kitchen_identifier = result
+        ) THEN
+            RETURN result;
+        END IF;
+    END LOOP;
+END;
+$$;
